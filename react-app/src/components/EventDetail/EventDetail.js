cbe629dd1e6a6967067980c675f1cbb3ef25a832
usernameGio <giordan.maniti@gmail.com>
2022-09-30 13:48:34 -0400

    worked on event edge cases

--- react-app/src/components/EventDetail/EventDetail.js
+++ react-app/src/components/EventDetail/EventDetail.js
@@ -1,21 +1,43 @@
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, useHistory } from "react-router-dom";
+import { deleteEventById } from "../../store/event";
 import EditEventModal from "../EditEvent/EditEventModal";
 
 //dummy commit
 function EventDetail() {
   const { eventId } = useParams();
+  let currentUser;
+  const sessionUser = useSelector((state) => state.session.user);
+  const dispatch = useDispatch();
+  const history = useHistory();
   const event = useSelector((state) => state.event[eventId]);
   // const user = useSelector((state) => state.session.user);
+
+  async function handleDelete(e) {
+    e.preventDefault();
+    await dispatch(deleteEventById(eventId));
+    history.push("/events");
+  }
+
+  if (sessionUser && event) {
+    if (sessionUser.id === event.event_owner.id) {
+      currentUser = true;
+    } else currentUser = false;
+  }
 
   return (
     <div className="event-detail-container">
       <div className="event-detail-image">
         <img src={event?.preview_image} alt="event" />
       </div>
-      <div className="EditDeleteBusiness flex">
-        <EditEventModal />
-      </div>
+      {currentUser && (
+        <div className="Event-components">
+          <EditEventModal />
+          <button className="delete-event" onClick={handleDelete}>
+            Delete Event
+          </button>
+        </div>
+      )}
       <div className="event-detail-name">{event?.name}</div>
       <div className="event-detail-location">
         {event?.address} {event?.city}, {event?.state}, {event?.zipcode}
