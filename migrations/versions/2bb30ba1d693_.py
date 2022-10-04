673553116889a1dd1abdf7acd2c6d291f07bbc5e
usernameGio <giordan.maniti@gmail.com>
2022-10-04 13:28:00 -0400

    re seeding heroku

--- migrations/versions/2bb30ba1d693_.py
+++ migrations/versions/2bb30ba1d693_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: c27562262ab1
+Revision ID: 2bb30ba1d693
 Revises: 
-Create Date: 2022-10-03 11:22:33.118371
+Create Date: 2022-10-04 13:26:30.193978
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'c27562262ab1'
+revision = '2bb30ba1d693'
 down_revision = None
 branch_labels = None
 depends_on = None
--- react-app/src/components/Reviews/CreateReview/ReviewModal.js
+++ react-app/src/components/Reviews/CreateReview/ReviewModal.js
@@ -13,8 +13,7 @@ function ReviewModal({ event }) {
     <>
       <button
         className="review-button"
-        onClick={(e) => {
-          e.stopImmediatePropagation();
+        onClick={() => {
           setShowModal(true);
         }}
       >
