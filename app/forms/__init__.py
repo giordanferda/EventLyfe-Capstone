12fe2dca0e677ab6860995d06937e94d05ad4bfa
usernameGio <giordan.maniti@gmail.com>
2022-10-27 21:58:38 -0400

    ticket form and model

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -3,3 +3,4 @@ from .signup_form import SignUpForm
 from .event_form import EventForm
 from .review_form import ReviewForm
 from .like_form import LikeForm
+from .ticket_form import TicketForm
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,4 @@ from .user import User
 from .events import Event
 from .review import Review
 from .likes import Like
+from .ticket import Ticket
